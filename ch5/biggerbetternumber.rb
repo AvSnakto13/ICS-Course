11481f9731f1cc9a55a055a877a0b7476dcaa5e0
AvSnakto13 <andrewb152010@hotmail.com>
2017-06-08 10:55:07 -0700

    week 3 assignments

---
+++ ch5/biggerbetternumber.rb
@@ -0,0 +1,5 @@
+puts 'hail sir or madam, what be thy favorite numeral?'
+numeral = gets.chomp
+nu = 1
+nunumeral = numeral.to_i+nu
+puts 'hmmm, an excellent figure indeed, but what think you of '+nunumeral.to_s+' as a more massive, greater figure?' 
---
+++ ch5/fullnamegreeting.rb
@@ -0,0 +1,8 @@
+puts 'Salutations, what might I call, thee?'
+firstname = gets.chomp
+puts 'and have thee a second name?'
+middlename = gets.chomp 
+puts 'and what be thy clan or house name?'
+lastname = gets.chomp
+puts 'well good day to you ' +firstname+' '+middlename+' '+lastname+' it was a pleasure to make your acquaintance'
+  
