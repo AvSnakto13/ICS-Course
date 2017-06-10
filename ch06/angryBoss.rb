ea5712a3551211c06f50b218273058ccbb4fe5b8
AvSnakto13 <andrewb152010@hotmail.com>
2017-06-10 12:17:46 -0700

    chapter 6 exercises

---
+++ ch06/angryBoss.rb
@@ -0,0 +1,3 @@
+puts 'Shmee, what in Pan\'s blasted feather has made you come to my quarters while you\'re supposed to be out finding Peter Pan you uselessly insufferable seadog?'
+response = gets.chomp
+puts 'you want what!?' +response+ ' why you ungrateful bucket of crocodile excrement, you\'re fired'
---
+++ ch06/tableOfContents.rb
@@ -0,0 +1,35 @@
+line_width = 75
+table = 'Table of Contents' 
+line1 = 'Chapter 1:  Ruby Rose' 
+pg1 = 'page   1'
+line2 = 'Chapter 2:  The Shining Beacon'
+pg2 = 'page  13'
+line3 = 'Chapter 3:  The First Step'
+pg3 = 'page  39'
+line4 = 'Chapter 4:  The Emerald Forest'
+pg4 = 'page  51'
+line5 = 'Chapter 5:  Players and Pieces'
+pg5 = 'page  75'
+line6 = 'Chapter 6:  The Badge and the Burden'
+pg6 = 'page  81'
+line7 = 'Chapter 7:  Jaundice'
+pg7 = 'page  89'
+line8 = 'Chapter 8:  Forever Fall'
+pg8 = 'page 111'
+line9 = 'Chapter 9:  The Stray'
+pg9 = 'page 131'
+line10 = 'Chapter 10: Black and White'
+pg10 = 'page 151'
+puts    
+puts (table.center(line_width))
+puts   
+puts (line1.ljust(line_width/2) + pg1.rjust(line_width/2))
+puts (line2.ljust(line_width/2) + pg2.rjust(line_width/2))
+puts (line3.ljust(line_width/2) + pg3.rjust(line_width/2))
+puts (line4.ljust(line_width/2) + pg4.rjust(line_width/2))
+puts (line5.ljust(line_width/2) + pg5.rjust(line_width/2))
+puts (line6.ljust(line_width/2) + pg6.rjust(line_width/2))
+puts (line7.ljust(line_width/2) + pg7.rjust(line_width/2))
+puts (line8.ljust(line_width/2) + pg8.rjust(line_width/2))
+puts (line9.ljust(line_width/2) + pg9.rjust(line_width/2))
+puts (line10.ljust(line_width/2) + pg10.rjust(line_width/2))
